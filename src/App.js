8e768f916e39f8b08572741f86f3613cceea2d2f
Naim <skylike2904@gmail.com>
2022-07-27 01:45:04 +0600

    added loading screen

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ClockLoader from "react-spinners/ClockLoader";
 
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
@@ -6,12 +7,29 @@ import HeadlineCards from "./components/HeadlineCards/HeadlineCards";
 import Umbrellas from "./components/Umbrellas/Umbrellas";
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   return (
     <div>
-      <Navbar />
-      <Hero />
-      <HeadlineCards />
-      <Umbrellas />
+      {loading ? (
+        <div className="max-w-[1640px] h-screen p-4 mx-auto flex items-center justify-center bg-blue-900">
+          <ClockLoader color={"#fff"} loading={loading} size={150} />
+        </div>
+      ) : (
+        <>
+          <Navbar />
+          <Hero />
+          <HeadlineCards />
+          <Umbrellas />
+        </>
+      )}
     </div>
   );
 };
